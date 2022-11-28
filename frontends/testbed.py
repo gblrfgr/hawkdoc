998de79dd516ce914bbd12afa8f46e40cec67fff
June Mills <gblrfgr@gmail.com>
2022-11-28 00:24:59 -0600

    added testbed for frontends

---
+++ frontends/testbed.py
@@ -0,0 +1,26 @@
+import importlib
+import collections
+import inspect
+import types
+
+
+Declaration = collections.namedtuple("Declaration", ["name", "value"])
+
+
+def load_module(pathname: str) -> types.ModuleType:
+    try:
+        mod_name = inspect.getmodulename(pathname)
+        if not mod_name:
+            raise ValueError("couldn't determine module name")
+        mod = importlib.import_module(mod_name)
+    except Exception as e:
+        raise ValueError(f'couldn\'t load module at "{pathname}" (reason: "{e}")')
+    return mod
+
+
+def get_top_levels(mod: types.ModuleType) -> list[Declaration]:
+    return [
+        Declaration(name=tld, value=getattr(mod, tld))
+        for tld in mod.__dict__.keys()
+        if (tld not in __builtins__.__dict__ and not tld.startswith("_"))
+    ]
